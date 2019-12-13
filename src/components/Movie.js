0c5c0bba3eb8b0eaf0ccea5f62d82f7001265d24
이상민 <leesangmin@isangmin-ui-MacBook-Pro.local>
2019-12-13 16:07:56 +0900

    현재까지

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
 function Movie( {year , title , summary , poster , genres }) {
-return <div className="movie">
+return (
+        <Link to={{
+            pathname : "/movie-detail",
+            state : {
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}>
+        <div className="movie">
             <img src={poster} alt={poster} title={poster} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
@@ -18,7 +30,9 @@ return <div className="movie">
                 </ul>
             </div>
         
-</div>
+        </div>
+        </Link>
+    );
 }
 
 Movie.propTypes = {
---
+++ src/routes/Detail.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Detail(props){
+    console.log(props);
+    return <span>hhhh</span>
+}
+
+export default Detail;
